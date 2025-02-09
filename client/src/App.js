9457e0a37d77853e5ada12527cee13f576962790
ssussu <ssussu00623@gmail.com>
2025-02-09 18:47:10 +0900

    member

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Layout from'./pages/Layout.jsx';
 import KurlyMain from'./pages/KurlyMain.jsx';
 import Detail from'./pages/Detail.jsx';
 import ProductList from './pages/ProductList.jsx';
+import Login from './component/member/Login.jsx';
+import Signup from './component/member/Signup.jsx';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
               <Route index element={<KurlyMain />} />
               <Route path="/goods/list" element={<ProductList />} />
               <Route path="/goods/detail" element={<Detail />} />
+              <Route path="/memeber/login" element={<Login />} />
+              <Route path="/memeber/ㄴignup" element={<Signup />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ client/src/component/member/Login.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link  } from 'react-router-dom'; 
+import '../../scss/member.scss'
+import Signup from './Signup.jsx';
+
+export default function Login() {
+    return (
+        <div className='content'> 
+                <div className='form-box'>
+            <div className='login-box'>
+                <div className='member-title-box'>
+                    <span>
+                        로그인
+                    </span>
+                </div>
+                    <form>
+                        <ul>
+                            <li>
+                                <div>
+                                    <input 
+                                        className='loginform-input'
+                                        type="text"
+                                        placeholder='아이디를 입력해주세요' />
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <input 
+                                        className='loginform-input'
+                                        type="password"
+                                        placeholder='비밀번호를 입력해주세요' />
+                                </div>
+                            </li>
+                            <li>
+                                <div className='login-founder'>
+                                <a href="#">아이디 찾기</a> 
+                                <span>&gt;</span>
+                                <a href="#">패스워드 찾기</a> 
+                                <span>&gt;</span>
+                                </div>
+                            </li>
+                            <li>
+                                <button>로그인</button>
+                            </li>
+                            <li>
+                                <button>
+                                    <Link to={'/member/signup'}>
+                                    회원가입
+                                    </Link>
+                                </button>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+                {/* form-box end */}
+            </div> 
+            {/* Loginbox end */}
+        </div> // content end 
+    );
+}
