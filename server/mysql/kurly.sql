8556f6548be9bd2761b8debd78d56a66a3317414
kim <yellsdesign@naver.com>
2025-02-21 18:01:45 +0900

    product

--- server/mysql/kurly.sql
+++ server/mysql/kurly.sql
@@ -24,3 +24,22 @@ create table kurly_product(
 );
 desc kurly_product;
 select * from  kurly_product;
+create table kurly_product(
+	pid 			int 			primary key 	auto_increment,
+    brend			varchar(20)		not null,
+    cate_depth1		char(3) 		not null,
+    cate_depth2		char(3) 		not null,
+    subject			varchar(100)	not null,
+    sub_desc		varchar(100) ,
+    price 			int  			not null,
+    dc				int,
+    delivery		char(2)	,
+    event_label		boolean,
+    upload_img		varchar(100) ,
+    org_img 		varchar(50) ,
+	info_imgs 		json ,
+    info_org_imgs 	json,
+    detail_imgs 	json ,
+    detail_org_imgs json,
+    pdate 			datetime
+);
