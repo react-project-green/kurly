61707e4865e305061bf69ea7b6315a11046242b3
ssussu <ssussu00623@gmail.com>
2025-02-26 10:31:00 +0900

    ==member(server pwd)

--- server/repository/db.js
+++ server/repository/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'root/mysql1234',
+    password: 'mysql1234',
     database: 'kurlydb',
 });
 
